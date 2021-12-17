62f6a4e451dbb9272a2b99f3189a32784c619872
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-17 13:30:06 +0700

    add: implement web scope;

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,22 +1,6 @@
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
-import com.pluralsight.repository.SpeakerRepository;
-import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class AppConfig {
-    @Bean(name = "speakerService")
-    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
-    public SpeakerService getSpeakerService() {
-        return new SpeakerServiceImpl();
-    }
-
-    @Bean(name = "speakerRepository")
-    public SpeakerRepository getSpeakerRepository() {
-        return new HibernateSpeakerRepositoryImpl();
-    }
-}
+@ComponentScan({"com.pluralsight"})
+public class AppConfig { }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,18 +1,22 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
-    public SpeakerServiceImpl() {}
+    public SpeakerServiceImpl() {
+    }
 
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("speakerRepository constructor");
         this.repository = speakerRepository;
     }
 
@@ -21,7 +25,6 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
-    @Autowired
     public void setRepository(SpeakerRepository speakerRepository) {
         this.repository = speakerRepository;
     }
