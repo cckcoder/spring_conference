1a49cdc42e55871daab24e315ab56df75092b7f2
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-17 10:22:55 +0700

    add: single-ton pattern;

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,9 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(service.findAll().get(0).toString());
+        System.out.println(service);
+//        System.out.println(service.findAll().get(0).toString());
+        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
     }
 }
